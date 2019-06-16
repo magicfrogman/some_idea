68c30c5b4c0300e7fc0621536e9daacf600df956
magicfrogman <wind1982@163.com>
2019-06-16 23:43:32 +0800

    Create scapy.py
    简单的流量监控

---
+++ scapy.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 20:57:36 2019
+scapy test
+@author: zhangtong
+"""
+from scapy.all import *
+import time
+
+
+z = [] #初始化一个序列
+def sniff_prn(package):
+    src = package['IP'].src
+    dst = package['IP'].dst
+    sport = package['TCP'].sport
+    dport = package['TCP'].dport
+    z.append([src,dst,sport,dport])
+
+#同步
+#sniff(filter="tcp and ( port 80 or port 110 )",
+#        #过滤条件，可以写成函数过滤，也可以bpf语法
+#            prn=sniff_prn,#回调函数
+#            count = 10)
+
+#=============================================================================
+
+
+#异步
+t = AsyncSniffer(filter="tcp and ( port 80 or port 110 )",
+        #过滤条件，可以写成函数过滤，也可以bpf语法
+            prn=sniff_prn,#回调函数
+          )
+t.start()
+time.sleep(5) # 5秒内走过的流量
+t.stop()
+
+#=============================================================================
+
