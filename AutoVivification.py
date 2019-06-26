db09144a4acdc41a0a1ab80b6115fa6684d05d8c
magicfrogman <wind1982@163.com>
2019-06-26 16:42:25 +0800

    Create AutoVivification.py

---
+++ AutoVivification.py
@@ -0,0 +1,10 @@
+﻿class AutoVivification(dict):
+    """Implementation of perl's autovivification feature.
+    一个任意深度迭代的类，大大简化了建树过程，666
+    """
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
